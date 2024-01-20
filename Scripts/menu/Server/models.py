d1a2c77305f430b9ef5960ccf2e02b3c724c49be
SunnyWen0422 <146903366+SunnyWen0422@users.noreply.github.com>
2024-01-20 15:25:41 +0800

    Add files via upload

---
+++ Scripts/menu/Server/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class dish(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
+    class Meta:
+        db_table = "caidan"
+
